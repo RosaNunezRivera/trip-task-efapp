6654ba8020ba7922d29171de38ce8dd1cb8f0ce0
Wellington Suero <144365343+Wellfc@users.noreply.github.com>
2024-03-01 14:39:24 -0600

    Add project files.

---
+++ Controllers/TripController.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using IPBLL;
+using IPDAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TripTaskEFApp.Models;
+
+namespace TripTaskEFApp.Controllers
+{
+    public class TripController : Controller
+    {
+        // GET: Trip
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetTrips()
+        {
+            TripService tripService = new TripService();
+            var trips = tripService.GetTrips();
+            List<TripVM> tripVMs = new List<TripVM>();
+
+            foreach(Trip vm in trips)
+            {
+                tripVMs.Add(new TripVM
+                {
+                    TripId = vm.TripId,
+                    Description = vm.Description,
+                    Destination = vm.Destination,
+                    StartDate = vm.StartDate,
+                    EndDate = vm.EndDate
+                });
+            }
+
+            //Mapper.Initialize(x => x.CreateMap<Trip, TripVM>());
+            //Mapper.Map(trips, tripVMs);
+
+            return Json(tripVMs, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult DeleteTrip(int tripId)
+        {
+            TripService tripService = new TripService();
+            if(tripService.DeleteTrip(tripId))
+            {
+                return Json(JsonRequestBehavior.AllowGet);
+            }
+            return null;
+        }
+    }
+}
---
+++ IPDAL/TripRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPDAL
+{
+    public class TripRepository
+    {
+        public List<Trip> GetTripRepo()
+        {
+            ItineraryPlannerEntities ipe = new ItineraryPlannerEntities();
+            return ipe.Trips.ToList();
+        }
+
+        public bool DeleteTripRepo(int tripId)
+        {
+            ItineraryPlannerEntities ipe = new ItineraryPlannerEntities();
+            var trip = ipe.Trips.Where(x => x.TripId == tripId).FirstOrDefault();
+            if (trip != null)
+            {
+                ipe.Trips.Remove(trip);
+                ipe.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+    }
+}
